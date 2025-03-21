46ef85f529f283e69a6abeeeb8a8d937d9f64ad5
kionling <djvdjv209@gmail.com>
2025-03-20 22:21:45 -0700

    Enabling CORS middle ware to fetch from frontnd

--- api.py
+++ api.py
@@ -5,3 +5,12 @@ import time
 
 from data import sensor_data, simulate_sensor_reading
 
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
